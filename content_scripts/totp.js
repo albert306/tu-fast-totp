a9690aca3923bdd558b8c10cec1e9ee6097f5450
awolf <a.wolf04@protonmail.com>
2024-01-05 16:41:59 +0100

    working version with insert token button

--- content_scripts/totp.js
+++ content_scripts/totp.js
@@ -1,20 +1,18 @@
 (() => {
-    function enterTotp() {
+    function enterTotp(code) {
         const totpInput = document.querySelector('input[id="fudis_otp_input"]')
         const submitButton = document.querySelector('button[name="_eventId_proceed"]')
+
+        console.error("Code " + code + "was received")
         
-        // totpInput.value = new TotpGenerator().getOTP(secret)
-        totpInput.value = "test"
-        submitButton.click
+        totpInput.value = code
+        submitButton.click()
 
     }
 
     browser.runtime.onMessage.addListener((message) => {
-        console.log("reached onmessage listener")
-        if (message.command === "enter") {
-            enterTotp(message.beastURL);
-        } else if (message.command === "reset") {
-            
+        if (message.command === "enterTotpCode") {
+            enterTotp(message.code);
         }
-      })
+    })
 })()
--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,24 @@
 function listenForClicks() {
-    console.log("listening")
     document.addEventListener("click", (e) => {
-        generateTotp()  
+
+        function sendTimeCode(tabs) {
+
+            var totp = new jsOTP.totp()
+            var timeCode = totp.getOtp("EZASM2WUXRZS6UCFWMAEXR6WMYXU2QII")
+
+            browser.tabs.sendMessage(tabs[0].id, {
+                command: "enterTotpCode",
+                code: timeCode,
+            })
+        }
+
+        browser.tabs.query({active: true, currentWindow: true})
+            .then(sendTimeCode)
+        
     })
 }
 
+browser.tabs.executeScript({file: "/content_scripts/totp.js"})
+    .then(listenForClicks)
+    .catch(() => { console.log("failed to load") });
 
-function generateTotp() {
-    var totpObj = new TOTP()
-    var otp = totpObj.getOTP("SECRET_HERE")
-    var textoutput = document.getElementById("popupTotpText")
-    textoutput.value = otp
-}
-
-browser.tabs
-  .executeScript({ file: "/content_scripts/totp.js" })
-  .then(listenForClicks)
-  .catch(console.log("content_script loading has failed"))
